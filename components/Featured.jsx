fcc80f9f0a90ef960113ed11ab4b8786a871cf7e
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2022-04-18 18:52:15 -0400

    navbar changed

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -12,19 +12,19 @@ const Featured = () => {
 
 
   // navbar pictures slider
-  const handleArrow = (direction) =>{
-    if(direction==="left"){
-        setIndex(index !== 0 ? index-1 : 2)
-    }
-    if(direction==="right"){
-        setIndex(index !== 2 ? index+1 : 0)
-    }
-}
+//   const handleArrow = (direction) =>{
+//     if(direction==="left"){
+//         setIndex(index !== 0 ? index-1 : 2)
+//     }
+//     if(direction==="right"){
+//         setIndex(index !== 2 ? index+1 : 0)
+//     }
+// }
   return (
     <div className={styles.container}>
-      <div className={styles.arrowContainer} style={{ left: 0 }} onClick={()=>handleArrow("left")}>
+      {/* <div className={styles.arrowContainer} style={{ left: 0 }} onClick={()=>handleArrow("left")}>
         <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain" />
-      </div>
+      </div> */}
 
       <div className={styles.wrapper} style={{transform: `translateX(${-100*index}vw)`}}>
         {images.map((img, i) => (
@@ -34,9 +34,9 @@ const Featured = () => {
           </div>
         ))}
       </div>
-      <div className={styles.arrowContainer} style={{ right: 0 }} onClick={()=>handleArrow("right")}>
+      {/* <div className={styles.arrowContainer} style={{ right: 0 }} onClick={()=>handleArrow("right")}>
         <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain" />
-      </div>
+      </div> */}
 
 
     </div>
